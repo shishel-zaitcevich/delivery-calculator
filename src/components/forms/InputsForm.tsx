6aaa8aea6620bcf4b2058feaafb018660a39fe83
shishel-zaitcevich <ushka_ya@mail.ru>
2023-03-13 03:05:17 +0300

    feat: mobile version

--- src/components/forms/InputsForm.tsx
+++ src/components/forms/InputsForm.tsx
@@ -31,36 +31,43 @@ export function NumberForms() {
 
   return (
     <>
-      <div className={'container'}>
-        <img src={require('../../assets/Take_Away.gif')} alt="gif..." />
-        <form onSubmit={handleSubmit(submitHandler)}>
-          <Paper
-            style={{
-              display: 'grid',
-              gridRowGap: '20px',
-              padding: '20px',
-              margin: '10px 150px',
-            }}
-          >
-            <Typography variant="h6"> Delivery fee calculator</Typography>
+      <div className="container">
+        <div className="gif">
+          <img className="gif" src={require('../../assets/Take_Away.gif')} alt="gif..." />
+        </div>
+        <div className="form">
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <Paper
+              className="paper"
+              style={{
+                display: 'grid',
+                gridRowGap: '20px',
+                padding: '20px',
+                // margin: '10px 150px',
+              }}
+            >
+              <Typography variant="h6"> Delivery fee calculator</Typography>
 
-            <FormInputText name="orderPrice" control={control} label="Cart value" />
-            <FormInputText name="distance" control={control} label="Delivery distance" />
-            <FormInputText name="quantity" control={control} label="Amount of items" />
-            <BasicDateTimePicker name="dateTime" label="" control={control} />
+              <FormInputText name="orderPrice" control={control} label="Cart value" />
+              <FormInputText name="distance" control={control} label="Delivery distance" />
+              <FormInputText name="quantity" control={control} label="Amount of items" />
+              <BasicDateTimePicker name="dateTime" label="" control={control} />
 
-            <Button onClick={handleSubmit(submitHandler)} variant={'contained'}>
-              {' '}
-              Calculate delivery price{' '}
-            </Button>
-            <Button onClick={() => reset()} variant={'outlined'}>
-              {' '}
-              Reset{' '}
-            </Button>
+              <Button onClick={handleSubmit(submitHandler)} variant={'contained'}>
+                {' '}
+                Calculate delivery price{' '}
+              </Button>
+              <Button onClick={() => reset()} variant={'outlined'}>
+                {' '}
+                Reset{' '}
+              </Button>
 
-            <Typography variant="h4">Delivery fee is {values.deliveryFee.toFixed(2)} €</Typography>
-          </Paper>
-        </form>
+              <Typography variant="h4">
+                Delivery fee is {values.deliveryFee.toFixed(2)} €
+              </Typography>
+            </Paper>
+          </form>
+        </div>
       </div>
     </>
   );
